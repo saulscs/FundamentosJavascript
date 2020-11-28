a5d6962ddfdabcc48ad7b8faecdd08a782326ae9
Saul Corona <saulcoronasantos@ymail.com>
2020-11-27 19:48:31 -0600

    Styled Components & Single File Components 157

--- spa/app/components/Router.js
+++ spa/app/components/Router.js
@@ -3,6 +3,7 @@ import {ajax} from '../helpers/ajax.js'
 import { PostCard } from './PostCard.js';
 import { Post } from './Post.js';
 import { SearchCard } from './SearchCard.js';
+import { ContactForm } from './ContactForm.js';
 
 export async function Router(){
     const d = document,
@@ -54,7 +55,8 @@ export async function Router(){
         })
         
     } else if (hash === "#/contact"){
-        $main.innerHTML = "<h2>Sección de contacto</h2>";
+       
+        $main.appendChild(ContactForm());
        
     } else{
         await ajax({
